6fe78b44bdf23ab6474c71f37295a1630cf68dd4
dotNETDev-Zayniddin <dotnetdev2000@gmail.com>
2024-10-05 16:06:48 +0500

    Vazifa: Task Upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Vazifa
+{   
+    class Program
+    {
+        static void Main(){
+        
+            int x = 15;
+            int y = 10;
+            string result;
+            if(x>y){
+                System.Console.WriteLine("x is greater than y");
+            } 
+            else if(x<y){
+                System.Console.WriteLine("x is less than y");
+            }
+            else if( x == y){
+                System.Console.WriteLine("x is equal to y");
+            }
+            else{
+                System.Console.WriteLine("x and y are not comparable");
+            }
+        }
+    }
+}
